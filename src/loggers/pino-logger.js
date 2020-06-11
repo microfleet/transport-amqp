9974fda2a97a2823f7ea69e098aa5dbd8ac22311
Vitaly Aminev <AVVS@users.noreply.github.com>
2020-06-11 15:04:37 -0700

    fix: logging EAGAIN error (#89)

--- src/loggers/pino-logger.js
+++ src/loggers/pino-logger.js
@@ -1,20 +1,15 @@
 const pino = require('pino');
-const stdout = require('stdout-stream');
+const SonicBoom = require('sonic-boom');
 
 // holy crap with stdout and good
 const isProduction = process.env.NODE_ENV === 'production';
-const { write } = stdout;
-stdout.write = (chunk, enc, next) => write.call(stdout, chunk, enc, next);
 
 module.exports = (name = '@microfleet/transport-amqp', settings = {}) => {
   const opts = {
     name,
     level: isProduction ? 'info' : 'trace',
-    serializers: {
-      err: pino.stdSerializers.err,
-    },
     ...settings,
   };
 
-  return pino(opts, stdout);
+  return pino(opts, new SonicBoom({ fd: process.stdout.fd }));
 };
